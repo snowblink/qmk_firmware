f3b1d282ea0993ad5eac918589048893ad50ecfc
Jonathan Lim <snowblink@gmail.com>
2017-07-28 14:01:39 +0100

    Make toggle only require 2 taps

---
+++ keyboards/hhkb/keymaps/snowblink/config.h
@@ -0,0 +1,11 @@
+// Based off of this section:
+// https://github.com/qmk/qmk_firmware/blob/master/doc/BUILD_GUIDE.md#the-configh-file
+#ifndef CONFIG_SNOWBLINK_H
+#define CONFIG_SNOWBLINK_H
+
+// Bring in original defaults
+#include "../../config.h"
+
+#define TAPPING_TOGGLE 2 // how many taps before triggering the toggle
+
+#endif
