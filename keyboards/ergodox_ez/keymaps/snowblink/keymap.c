298e2719d1d530950501897936e47ea651a7797b
Jonathan Lim <snowblink@gmail.com>
2018-01-10 15:49:12 +0000

    Remove caps lock

--- keyboards/ergodox_ez/keymaps/snowblink/keymap.c
+++ keyboards/ergodox_ez/keymaps/snowblink/keymap.c
@@ -3,21 +3,12 @@
 #include "action_layer.h"
 #include "version.h"
 
-
-#include "keymap_german.h"
-
-#include "keymap_nordic.h"
-
-
-
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
   EPRM,
   VRSN,
   RGB_SLD,
-
 };
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -26,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,LGUI(KC_C),
     CTL_T(KC_ESCAPE),KC_A,KC_S,KC_D,KC_F,KC_G,
     KC_LSFT,KC_Z,KC_X,KC_C,KC_V,KC_B,LGUI(KC_V),
-    KC_CAPSLOCK,KC_TRANSPARENT,KC_LCTL,KC_LALT,KC_LGUI,
+    KC_TRANSPARENT,KC_TRANSPARENT,KC_LCTL,KC_LALT,KC_LGUI,
     KC_MINUS,KC_EQUAL,KC_TRANSPARENT,
     KC_LGUI,KC_ENTER,
     LCTL(LGUI(KC_D)),
