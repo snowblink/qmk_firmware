ace87c7ef2a208370a6e2cf92d8c6f93383575c7
Jonathan Lim <snowblink@gmail.com>
2018-01-17 15:38:47 +0000

    Add keymap for minivan

--- keyboards/tv44/keymaps/snowblink/keymap.c
+++ keyboards/tv44/keymaps/snowblink/keymap.c
@@ -9,10 +9,9 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
-#define _QW 0
-#define _L1 3
-#define _L2 4
-#define _L3 5
+#define BASE 0
+#define UPPER 1
+#define NUMPAD 2
 
 // Curly braces have their own keys. These are defined to make them not mess up
 // the grid in layer 2.
@@ -24,30 +23,24 @@ extern keymap_config_t keymap_config;
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = KEYMAP( /* Qwerty */
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    MO(_L1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, MO(_L1),
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_L2),
-    KC_LCTL, MO(_L2), KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_ESC,  TG(_L3)
+  [BASE] = KEYMAP( /* Qwerty */
+    KC_GRV(KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_HYPHEN,
+    TT(UPPER), _______, KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_RSFT,  TT(NUMPAD)
   ),
-  [_L1] = KEYMAP( /* LAYER 1 */
+  [UPPER] = KEYMAP( /* LAYER 1 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-    _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_DOWN, KC_UP,   KC_LEFT, KC_RGHT, _______,
-    _______, KC_ESC,  _______, KC_PSCR, _______, _______, _______, KC_MSTP, KC_MPLY, KC_MPRV, KC_MNXT, KC_RSFT,
+    _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  _______, _______, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, _______,
+    _______, KC_ESC,  _______, KC_PSCR, _______, _______, _______, KC_MSTP, KC_LBRC, KC_RBRC, KC_MNXT, KC_RSFT,
     _______, KC_LGUI, _______,                   _______, _______,                   _______, _______, _______
   ),
-  [_L2] = KEYMAP( /* LAYER 2 */
+  [NUMPAD] = KEYMAP( /* LAYER 2 */
     _______, _______, _______, _______, _______, _______, _______, KC_7,    KC_8,    KC_9,    KC_0,    _______,
     KC_ESC,  KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    KC_VOLU, KC_ENT,
     _______, _______, _______, _______, _______, _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_VOLD, _______,
     _______, _______, _______,                   _______, _______,                   _______, _______, _______
   ),
-  [_L3] = KEYMAP( /* LAYER 3 */
-    _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______,
-    KC_ESC,  _______, _______, _______, _______, _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
-    KC_LSFT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
-    _______, KC_LSFT, KC_B,                      KC_SPC,  KC_C,                      _______, _______, _______
-  )
 };
 
 const uint16_t PROGMEM fn_actions[] = {
