43802a02a9e1157947c069130aefe086b9318b05
Jonathan Lim <snowblink@gmail.com>
2018-01-18 09:15:28 +0000

    Tidy up-

--- keyboards/tv44/keymaps/snowblink/keymap.c
+++ keyboards/tv44/keymaps/snowblink/keymap.c
@@ -25,16 +25,16 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP( /* Qwerty */
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
-    TT(UPPER), _______, KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_RSFT,  TT(NUMPAD)
+    CTL_T(KC_ESCAPE),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MINS,
+    TT(NUMPAD), _______, KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_RSFT,  TT(UPPER)
   ),
 
   [UPPER] = KEYMAP( /* LAYER 1 */
-    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-    _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  _______, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, _______, _______,
-    _______, KC_ESC,  _______, KC_PSCR, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,
-    _______, KC_LGUI, _______,                   _______, _______,                   _______, _______, _______
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+    _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,
+    _______, _______, _______,                   _______, _______,                   _______, _______, _______
   ),
 
   [NUMPAD] = KEYMAP( /* LAYER 2 */
