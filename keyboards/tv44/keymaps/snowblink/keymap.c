d9e9ccae405d45d3aea4ea7c319ddc6535cc6cc1
Jonathan Lim <snowblink@gmail.com>
2018-02-03 19:36:44 +0000

    Make into arrow command

--- keyboards/tv44/keymaps/snowblink/keymap.c
+++ keyboards/tv44/keymaps/snowblink/keymap.c
@@ -23,25 +23,25 @@ extern keymap_config_t keymap_config;
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = KEYMAP( /* Qwerty */
+  [BASE] = KEYMAP_ARROW_COMMAND( /* Qwerty */
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTL_T(KC_ESCAPE),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_MINUS),
-    TT(NUMPAD), KC_LALT, KC_LGUI,                   KC_ENT,  KC_SPC,                    KC_RALT, KC_RSFT,  TT(UPPER)
+    TT(NUMPAD), KC_LALT, KC_LGUI, _______,      KC_ENT,  KC_SPC,             _______, KC_RALT, KC_RSFT,  TT(UPPER)
   ),
 
-  [UPPER] = KEYMAP( /* LAYER 1 */
+  [UPPER] = KEYMAP_ARROW_COMMAND( /* LAYER 1 */
     KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
     _______, KC_BSLS, KC_QUOT, KC_MINS, KC_EQL,  KC_QUES, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______,
-    _______, _______, _______,                   _______, _______,                   _______, _______, _______
+    _______, _______, _______, _______,          _______, _______,          _______, _______, _______, _______
   ),
 
-  [NUMPAD] = KEYMAP( /* LAYER 2 */
+  [NUMPAD] = KEYMAP_ARROW_COMMAND( /* LAYER 2 */
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     KC_ESC,  KC_PIPE, KC_DQUO, KC_UNDS, KC_PLUS, L_CURBR, R_CURBR, KC_4,    KC_5,    KC_6,    KC_VOLU, KC_ENT,
     _______, _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,    KC_VOLD, _______,
-    _______, _______, _______,                   _______, KC_0,                      _______, _______, _______
+    _______, _______, _______, _______,          _______, KC_0,             _______, _______, _______, _______
   ),
 };
 
