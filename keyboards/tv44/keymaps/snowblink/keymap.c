e37a4ecb99235b01d5afeb4640d680ad680709f4
Jonathan Lim <jonlim@notonthehighstreet.com>
2020-02-22 12:29:43 +0000

    latest updates

--- keyboards/tv44/keymaps/snowblink/keymap.c
+++ keyboards/tv44/keymaps/snowblink/keymap.c
@@ -25,10 +25,10 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = KEYMAP_ARROW_COMMAND( /* Qwerty */
-    KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    CTL_T(KC_ESCAPE),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_MINUS),
-    TT(UPPER), KC_LCTL, KC_LALT, KC_LGUI,      KC_ENT,  KC_SPC,             TT(NUMPAD), _______, _______,  MO(ARROWS)
+    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+    CTL_T(KC_ESCAPE), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,    KC_COMM, KC_DOT, KC_SLSH, SFT_T(KC_MINUS),
+    MO(NUMPAD), KC_LCTL, KC_LALT, KC_LGUI, KC_ENT,  KC_SPC, TT(UPPER), _______, _______,  MO(ARROWS)
   ),
 
   [UPPER] = KEYMAP_ARROW_COMMAND( /* LAYER 1 */
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [ARROWS] = KEYMAP_ARROW_COMMAND( /* LAYER 3 */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, KC_PGDOWN,
     _______, _______, _______, _______,          _______, _______,          KC_LEFT, KC_DOWN, KC_RIGHT, _______
   ),
 };
