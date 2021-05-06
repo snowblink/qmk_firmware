0d115e5d7e5fba6ec0ebbba5676a56ae2f9334e0
Jonathan Lim <jonlim@notonthehighstreet.com>
2021-05-06 18:29:14 +0100

    Add sleep button

--- keyboards/ergodox_ez/keymaps/snowblink/keymap.c
+++ keyboards/ergodox_ez/keymaps/snowblink/keymap.c
@@ -6,213 +6,208 @@
 #define _______ KC_TRNS
 
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE, // can always be here
-  EPRM,
-  VRSN,
-  RGB_SLD,
+    PLACEHOLDER = SAFE_RANGE,  // can always be here
+    EPRM,
+    VRSN,
+    RGB_SLD,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  // base Layer
-  [0] = KEYMAP(
-    // left hand
-    KC_GRAVE,KC_1,KC_2,KC_3,KC_4,KC_5,TT(1), // top row
-    KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T, // 2nd row
-    LGUI(KC_C), // top big vertical
-    CTL_T(KC_ESCAPE),KC_A,KC_S,KC_D,KC_F,KC_G, // 3rd row
-    KC_LSFT,KC_Z,KC_X,KC_C,KC_V,KC_B, // 4th row
-    LGUI(KC_V), // bottom big vertical
-    TT(4), _______, KC_LCTL, KC_LALT, KC_LGUI, // bottom row
-    KC_MINUS,KC_EQUAL, // thumb top row
-    _______, // top thumblet
-    KC_LGUI,KC_ENTER, // big thumb buttons
-    _______, // bottom thumblet
+    // base Layer
+    [0] = KEYMAP(
+        // left hand
+        KC_GRAVE, KC_1, KC_2, KC_3, KC_4, KC_5, TT(1),   // top row
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,            // 2nd row
+        LGUI(KC_C),                                      // top big vertical
+        CTL_T(KC_ESCAPE), KC_A, KC_S, KC_D, KC_F, KC_G,  // 3rd row
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B,           // 4th row
+        LGUI(KC_V),                                      // bottom big vertical
+        TT(4), _______, KC_LCTL, KC_LALT, KC_LGUI,       // bottom row
+        KC_MINUS, KC_EQUAL,                              // thumb top row
+        _______,                                         // top thumblet
+        KC_LGUI, KC_ENTER,                               // big thumb buttons
+        _______,                                         // bottom thumblet
 
-    // right hand
-    TT(2),KC_6,KC_7,KC_8,KC_9,KC_0,KC_BSLASH, // top row
-    LGUI(KC_KP_PLUS), // top big vertical
-    KC_Y,KC_U,KC_I,KC_O,KC_P,KC_MINUS, // 2nd row
-    KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE, // 3rd row
-    LGUI(KC_KP_MINUS), // bottom big vertical
-    KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSFT, // 4th row
-    KC_LGUI,KC_LALT,KC_LCTL, _______, TT(3), // bottom row
-    KC_LBRACKET,KC_RBRACKET, // thumb top row
-    M(2), M(1), // two thumblets
-    KC_BSPACE,KC_SPACE // big thumb buttons
-  ),
+        // right hand
+        TT(2), KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLASH,   // top row
+        LGUI(KC_KP_PLUS),                                 // top big vertical
+        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINUS,           // 2nd row
+        KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE,      // 3rd row
+        LGUI(KC_KP_MINUS),                                // bottom big vertical
+        KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSFT,  // 4th row
+        KC_LGUI, KC_LALT, KC_LCTL, _______, TT(3),        // bottom row
+        KC_LBRACKET, KC_RBRACKET,                         // thumb top row
+        M(2), M(1),                                       // two thumblets
+        KC_BSPACE, KC_SPACE                               // big thumb buttons
+        ),
 
-  // layer 1
-  [1] = KEYMAP(
-    // left hand
-    M(0),KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,_______, // top row
-    _______,KC_EXLM,KC_AT,KC_LCBR,KC_RCBR,KC_PIPE, // 2nd row
-    _______, // top big vertical
-    _______,KC_HASH,KC_DLR,KC_LPRN,KC_RPRN,KC_GRAVE, // 3rd row
-    _______, // bottom big vertical
-    KC_PERC,KC_CIRC,KC_LBRACKET,KC_RBRACKET,KC_TILD,_______, // 4th row
-    _______,_______,_______,_______,_______, // bottom row
-    RGB_MOD,_______, // thumb row top
-    _______, // top thumblet
-    RGB_VAD,RGB_VAI, // big thumb buttons
-    _______, // bottom thumblet
+    // layer 1
+    [1] = KEYMAP(
+        // left hand
+        M(0), KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______,              // top row
+        _______, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE,            // 2nd row
+        _______,                                                       // top big vertical
+        _______, KC_HASH, KC_DLR, KC_LPRN, KC_RPRN, KC_GRAVE,          // 3rd row
+        _______,                                                       // bottom big vertical
+        KC_PERC, KC_CIRC, KC_LBRACKET, KC_RBRACKET, KC_TILD, _______,  // 4th row
+        _______, _______, _______, _______, _______,                   // bottom row
+        RGB_MOD, _______,                                              // thumb row top
+        _______,                                                       // top thumblet
+        RGB_VAD, RGB_VAI,                                              // big thumb buttons
+        _______,                                                       // bottom thumblet
 
-    // right hand
-    _______,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11, // top row
-    _______, // top big vertical
-    _______, _______,_______,_______,_______,_______, // 3rd row
-    _______, _______,_______,_______,_______,_______, // 2nd row
-    _______, // bottom big vertical
-    KC_AMPR, _______,_______,_______,_______,_______, // 4th row
-    _______, _______, _______, _______, _______, // bottom row
-    RGB_TOG,RGB_SLD, // thumb top row
-    _______,_______, // two thumblets
-    RGB_HUD,RGB_HUI // big thumb buttons
-  ),
+        // right hand
+        _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,   // top row
+        _______,                                               // top big vertical
+        _______, _______, _______, _______, _______, _______,  // 3rd row
+        _______, _______, _______, _______, _______, _______,  // 2nd row
+        _______,                                               // bottom big vertical
+        KC_AMPR, _______, _______, _______, _______, _______,  // 4th row
+        _______, _______, _______, _______, _______,           // bottom row
+        RGB_TOG, RGB_SLD,                                      // thumb top row
+        _______, _______,                                      // two thumblets
+        RGB_HUD, RGB_HUI                                       // big thumb buttons
+        ),
 
-  // layer 2
-  [2] = KEYMAP(
-    // left hand
-    _______,_______,_______,_______,_______,_______,_______, // top row
-    _______,_______,_______,KC_MS_UP,_______,_______, // 2nd row
-    _______, // top big vertical
-    _______, _______,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,_______, // 3rd row
-    _______,_______, _______,_______,_______,_______, // 4th row
-    _______, // bottom big vertical
-    _______,_______, _______, KC_MS_BTN1,KC_MS_BTN2, // bottom row
-    _______,_______, // thumb top row
-    _______, // top thumblet
-    KC_MS_BTN1, _______, // big thumb buttons
-    _______, // bottom thumblet
+    // layer 2
+    [2] = KEYMAP(
+        // left hand
+        _______, KC_SYSTEM_SLEEP, _______, _______, _______, _______, _______,  // top row
+        _______, _______, _______, KC_MS_UP, _______, _______,                  // 2nd row
+        _______,                                                                // top big vertical
+        _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, _______,         // 3rd row
+        _______, _______, _______, _______, _______, _______,                   // 4th row
+        _______,                                                                // bottom big vertical
+        _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2,                      // bottom row
+        _______, _______,                                                       // thumb top row
+        _______,                                                                // top thumblet
+        KC_MS_BTN1, _______,                                                    // big thumb buttons
+        _______,                                                                // bottom thumblet
 
-    // right hand
-    _______,_______,_______,_______,_______,_______,_______, // top row
-    _______, // top big vertical
-    _______,_______,KC_UP,_______,_______,_______, // 2nd row
-    _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,KC_MEDIA_PLAY_PAUSE, // 3rd row
-    _______, // bottom big vertical
-    _______,_______,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,_______,_______, // 4th row
-    KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,_______,_______, // bottom row
-    _______,_______, // top thumb row
-    _______,_______, // two thumblets
-    _______,KC_MS_BTN2 // big thumb buttons
-  ),
+        // right hand
+        _______, _______, _______, _______, _______, _______, _______,                 // top row
+        _______,                                                                       // top big vertical
+        _______, _______, KC_UP, _______, _______, _______,                            // 2nd row
+        _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_MEDIA_PLAY_PAUSE,             // 3rd row
+        _______,                                                                       // bottom big vertical
+        _______, _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, _______, _______,  // 4th row
+        KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE, _______, _______,           // bottom row
+        _______, _______,                                                              // top thumb row
+        _______, _______,                                                              // two thumblets
+        _______, KC_MS_BTN2                                                            // big thumb buttons
+        ),
 
-  // layer 3
-  [3] = KEYMAP(
-    // left hand
-    _______,_______,_______,_______,_______,_______,_______, // top row
-    _______,_______,_______,KC_UP,_______,_______, // 2nd row
-    _______, // top big vertical
-    _______, _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______, // 3rd row
-    _______,_______, _______,_______,_______,_______, // 4th row
-    _______, // bottom big vertical
-    _______,_______, _______, _______, _______, // bottom row
-    _______, _______, // thumb top row
-    _______, // top thumblet
-    LALT(LCTL(KC_LGUI)),_______, // big thumb buttons
-    _______, // bottom thumblet
+    // layer 3
+    [3] = KEYMAP(
+        // left hand
+        _______, _______, _______, _______, _______, _______, _______,  // top row
+        _______, _______, _______, KC_UP, _______, _______,             // 2nd row
+        _______,                                                        // top big vertical
+        _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,          // 3rd row
+        _______, _______, _______, _______, _______, _______,           // 4th row
+        _______,                                                        // bottom big vertical
+        _______, _______, _______, _______, _______,                    // bottom row
+        _______, _______,                                               // thumb top row
+        _______,                                                        // top thumblet
+        LALT(LCTL(KC_LGUI)), _______,                                   // big thumb buttons
+        _______,                                                        // bottom thumblet
 
-    // right hand
-    _______,_______,_______,_______,_______,_______,_______, // top row
-    _______, // top big vertical
-    _______,_______,KC_UP,_______,_______,_______, // 2nd row
-    _______, KC_LEFT,KC_DOWN,KC_RIGHT,_______,KC_PGUP, // 3rd row
-    _______, // bottom big vertical
-    _______,_______,_______,KC_UP,_______,KC_PGDOWN, // 4th row
-    _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______, // bottom row
-    _______,_______, // thumb top row
-    _______,_______, // two thumblets
-    LALT(LSFT(KC_UP)),LALT(LSFT(KC_DOWN)) // big thumb buttons
-  ),
+        // right hand
+        _______, _______, _______, _______, _______, _______, _______,  // top row
+        _______,                                                        // top big vertical
+        _______, _______, KC_UP, _______, _______, _______,             // 2nd row
+        _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, KC_PGUP,          // 3rd row
+        _______,                                                        // bottom big vertical
+        _______, _______, _______, KC_UP, _______, KC_PGDOWN,           // 4th row
+        _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______,                   // bottom row
+        _______, _______,                                               // thumb top row
+        _______, _______,                                               // two thumblets
+        LALT(LSFT(KC_UP)), LALT(LSFT(KC_DOWN))                          // big thumb buttons
+        ),
 
-  // layer 1
-  [4] = KEYMAP(
-    // left hand
-    _______, _______, _______, _______, _______, _______, _______, // top row
-    _______, _______, _______, _______, _______, _______, // 2nd row
-    _______, // top big vertical
-    _______, _______, _______, _______, _______, _______, // 3rd row
-    _______, // bottom big vertical
-    _______, _______, _______, _______, _______, _______, // 4th row
-    _______, _______, _______, _______, _______, // bottom row
-    _______, _______, // thumb row top
-    _______, // top thumblet
-    _______, _______, // big thumb buttons
-    _______, // bottom thumblet
+    // layer 1
+    [4] = KEYMAP(
+        // left hand
+        _______, _______, _______, _______, _______, _______, _______,  // top row
+        _______, _______, _______, _______, _______, _______,           // 2nd row
+        _______,                                                        // top big vertical
+        _______, _______, _______, _______, _______, _______,           // 3rd row
+        _______,                                                        // bottom big vertical
+        _______, _______, _______, _______, _______, _______,           // 4th row
+        _______, _______, _______, _______, _______,                    // bottom row
+        _______, _______,                                               // thumb row top
+        _______,                                                        // top thumblet
+        _______, _______,                                               // big thumb buttons
+        _______,                                                        // bottom thumblet
 
-    // right hand
-    _______, _______, _______, _______, _______, _______, _______, // top row
-    _______, // top big vertical
-    _______, KC_7, KC_8, KC_9, _______, _______, // 3rd row
-    _______, KC_4, KC_5, KC_6, _______, _______, // 2nd row
-    _______, // bottom big vertical
-    _______, KC_1, KC_2, KC_3, _______,_______, // 4th row
-    KC_0, KC_DOT, _______, _______, _______, // bottom row
-    _______,_______, // thumb top row
-    _______,_______, // two thumblets
-    _______,_______ // big thumb buttons
-  ),
+        // right hand
+        _______, _______, _______, _______, _______, _______, _______,  // top row
+        _______,                                                        // top big vertical
+        _______, KC_7, KC_8, KC_9, _______, _______,                    // 3rd row
+        _______, KC_4, KC_5, KC_6, _______, _______,                    // 2nd row
+        _______,                                                        // bottom big vertical
+        _______, KC_1, KC_2, KC_3, _______, _______,                    // 4th row
+        KC_0, KC_DOT, _______, _______, _______,                        // bottom row
+        _______, _______,                                               // thumb top row
+        _______, _______,                                               // two thumblets
+        _______, _______                                                // big thumb buttons
+        ),
 
 };
 
-const uint16_t PROGMEM fn_actions[] = {
-  [1] = ACTION_LAYER_TAP_TOGGLE(1)
-};
+const uint16_t PROGMEM fn_actions[] = {[1] = ACTION_LAYER_TAP_TOGGLE(1)};
 
 // leaving this in place for compatibilty with old keymaps cloned and re-compiled.
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-      switch(id) {
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
+    switch (id) {
         case 0:
-        if (record->event.pressed) {
-          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-        }
-        break;
+            if (record->event.pressed) {
+                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+            }
+            break;
         case 1:
-        if (record->event.pressed) {
-            SEND_STRING("=>");
-            return false;
-        }
-        break;
+            if (record->event.pressed) {
+                SEND_STRING("=>");
+                return false;
+            }
+            break;
         case 2:
-        if (record->event.pressed) {
-            SEND_STRING("notonthehighstreet");
-            return false;
-        }
-        break;
-      }
+            if (record->event.pressed) {
+                SEND_STRING("notonthehighstreet");
+                return false;
+            }
+            break;
+    }
     return MACRO_NONE;
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    // dynamically generate these.
-    case EPRM:
-      if (record->event.pressed) {
-        eeconfig_init();
-      }
-      return false;
-      break;
-    case VRSN:
-      if (record->event.pressed) {
-        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-      }
-      return false;
-      break;
-    case RGB_SLD:
-      if (record->event.pressed) {
-        rgblight_mode(1);
-      }
-      return false;
-      break;
-
-  }
-  return true;
+    switch (keycode) {
+        // dynamically generate these.
+        case EPRM:
+            if (record->event.pressed) {
+                eeconfig_init();
+            }
+            return false;
+            break;
+        case VRSN:
+            if (record->event.pressed) {
+                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+            }
+            return false;
+            break;
+        case RGB_SLD:
+            if (record->event.pressed) {
+                rgblight_mode(1);
+            }
+            return false;
+            break;
+    }
+    return true;
 }
 
 void matrix_scan_user(void) {
-
     uint8_t layer = biton32(layer_state);
 
     ergodox_board_led_off();
@@ -249,5 +244,4 @@ void matrix_scan_user(void) {
         default:
             break;
     }
-
 };
