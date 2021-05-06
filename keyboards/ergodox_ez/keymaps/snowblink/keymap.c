7a075f5af6dd276c66c390fff621b16a2fd020cf
Jonathan Lim <jonlim@notonthehighstreet.com>
2021-05-06 19:28:42 +0100

    Update to latest qmk

--- keyboards/ergodox_ez/keymaps/snowblink/keymap.c
+++ keyboards/ergodox_ez/keymaps/snowblink/keymap.c
@@ -181,65 +181,92 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        // dynamically generate these.
-        case EPRM:
-            if (record->event.pressed) {
-                eeconfig_init();
-            }
-            return false;
-            break;
-        case VRSN:
-            if (record->event.pressed) {
+    if (record->event.pressed) {
+        switch (keycode) {
+            case VRSN:
                 SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
-            }
-            return false;
-            break;
-        case RGB_SLD:
-            if (record->event.pressed) {
+                return false;
+#ifdef RGBLIGHT_ENABLE
+            case RGB_SLD:
                 rgblight_mode(1);
-            }
-            return false;
-            break;
+                return false;
+#endif
+        }
     }
     return true;
 }
 
-void matrix_scan_user(void) {
-    uint8_t layer = biton32(layer_state);
+// Runs just one time when the keyboard initializes.
+void keyboard_post_init_user(void) {
+#ifdef RGBLIGHT_COLOR_LAYER_0
+    rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+};
 
+// Runs whenever there is a layer state change.
+layer_state_t layer_state_set_user(layer_state_t state) {
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
+
+    uint8_t layer = get_highest_layer(state);
     switch (layer) {
+        case 0:
+#ifdef RGBLIGHT_COLOR_LAYER_0
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+            break;
         case 1:
             ergodox_right_led_1_on();
+#ifdef RGBLIGHT_COLOR_LAYER_1
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+#endif
             break;
         case 2:
             ergodox_right_led_2_on();
+#ifdef RGBLIGHT_COLOR_LAYER_2
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+#endif
             break;
         case 3:
             ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_3
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+#endif
             break;
         case 4:
             ergodox_right_led_1_on();
             ergodox_right_led_2_on();
+#ifdef RGBLIGHT_COLOR_LAYER_4
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+#endif
             break;
         case 5:
             ergodox_right_led_1_on();
             ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_5
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+#endif
             break;
         case 6:
             ergodox_right_led_2_on();
             ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_6
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+#endif
             break;
         case 7:
             ergodox_right_led_1_on();
             ergodox_right_led_2_on();
             ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_7
+            rgblight_setrgb(RGBLIGHT_COLOR_LAYER_7);
+#endif
             break;
         default:
             break;
     }
+
+    return state;
 };
